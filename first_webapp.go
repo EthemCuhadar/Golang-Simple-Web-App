0afe419360e068edb25792c1d56d5498e6171414
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-20 15:55:48 +0300

    Comments can be seen now

--- first_webapp.go
+++ first_webapp.go
@@ -4,8 +4,9 @@ import (
  "net/http"
 )
 
+// handler - Prints out "OK Let's Go" and URL path on the screen.
 func handler(writer http.ResponseWriter, request *http.Request) {
- fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+ fmt.Fprintf(writer, "OK Let's Go, %s!", request.URL.Path[1:])
 }
 
 func main() {
